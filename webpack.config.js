0402dc3502ac13e0204e24df84057a6f201a9ce9
xinlo <1311257306@qq.com>
2022-05-15 15:45:14 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    ...base,
+    devtool: "inline-source-map",
     devServer: {
-        static: './dist',
+        contentBase: "./dist"
     },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'My App',
-        template: 'src/assets/index.html'
-    })],
     module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }, ],
-    },
+        rules: [
+            ...base.module.rules,
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
 };
