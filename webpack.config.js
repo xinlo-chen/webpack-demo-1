841b27e6d6f90488e48421bd668d486781384560
xinlo <1311257306@qq.com>
2022-05-15 13:04:02 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
-    }
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }, ],
+    },
 };
